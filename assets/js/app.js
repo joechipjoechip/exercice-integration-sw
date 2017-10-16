4938a4efaed4dece9f479420ab125ce3b95a7793
Lionel O <lionel.orsini@hotmail.fr>
2017-10-16 20:58:51 +0200

    2nd global commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,10 +7,10 @@ import { log } from './log.js'
 // log(salut);
 
 
-document.getElementById('button').addEventListener('click', () => {
-  // import lazyload de jquery
-  import('jquery').then( ($) => {
-    $('body').css('backgroundColor', 'orange');
-  })  
-})
+// document.getElementById('button').addEventListener('click', () => {
+//   // import lazyload de jquery
+//   import('jquery').then( ($) => {
+//     $('body').css('backgroundColor', 'orange');
+//   })  
+// })
 
